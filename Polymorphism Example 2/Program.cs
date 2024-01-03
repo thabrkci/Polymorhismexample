3423b989088eee3526bbbf940c7762aca170cba0
thabrkci <alpacinotaha@gmail.com>
2024-01-03 11:03:46 +0300

    example2

---
+++ Polymorphism Example 2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Polymorphism_Example_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("WELCOME TO EXERCİSES 2!");
+        bodybuıld body = new bodybuıld();
+        body.lIfttheweIght();
+        Chest chest = new Chest();
+        chest.lIfttheweIght();
+        Shoulder shoulder = new Shoulder();
+        shoulder.lIfttheweIght();
+
+        
+    }
+
+}
+
+class bodybuıld
+{
+  public virtual void lIfttheweIght()
+  {
+    Console.WriteLine("Lift the weight");
+  }
+
+
+
+}
+
+class Chest:bodybuıld
+{
+    public override void lIfttheweIght()
+    {
+        //base.lIfttheweIght();
+        Console.WriteLine("Lift the weight with chest for chest");
+    }
+}
+
+class Shoulder:bodybuıld
+{
+  public override void lIfttheweIght()
+    {
+        base.lIfttheweIght();
+        Console.WriteLine("Lift the weight with shoulder");
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,45 @@
 ﻿namespace Polymorphism_ve_Sealed_Class;
 
+using System;
+
+// Temel sınıf (base class)
+class Sekil
+{
+    public virtual void Ciz()
+    {
+        Console.WriteLine("Bir şekil çiziliyor");
+    }
+}
+
+//Türetilmiş Sınıf
+class Dikdortgen : Sekil//Dikdörtgen isimli class Şekil sınıfından kalıtım almakta.
+{
+    public override void Ciz()
+    
+    {
+        Console.WriteLine("Dikdörtgen çiziliyor");
+    }
+}
+
+// Başka bir türetilmiş sınıf (derived class)
+class Daire : Sekil
+{
+    public override void Ciz()
+    {
+        Console.WriteLine("Daire çiziliyor");
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        // Polymorphism örneği
+        Sekil sekil1 = new Dikdortgen();
+        Sekil sekil2 = new Daire();
+
+        // Her iki durumda da Ciz metodu farklı şekilde çalışır
+        sekil1.Ciz(); // "Dikdörtgen çiziliyor"
+        sekil2.Ciz(); // "Daire çiziliyor"
     }
 }
